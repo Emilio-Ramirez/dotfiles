9925d443cabd5132457d1de52d4ed3ce0337f5de
Emilio-Ramirez <ramarm0825@gmail.com>
2023-03-09 18:00:58 -0600

    Second try

---
+++ nvim/lua/maps.lua
@@ -0,0 +1,21 @@
+local keymap = vim.keymap
+
+--Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement 
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Selecte all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n','te', ':tabedit<Return>', { silent = true })
+keymap.set('n', 'ss', ':split<Return><C-w>w', {silent = true})
+
+keymap.set('n' , 'sv', ':vsplit<Return><C-w>w', {silent = true})
+-- Move window 
