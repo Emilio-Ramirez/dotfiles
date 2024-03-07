f86511ffd24a10a416f39f8978f9dbf4cf8d06ec
Emilio2508 <emilio.ramirez@indexzero.io>
2024-03-07 09:48:55 -0600

    inicio

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -x PATH /usr/local/mysql/bin $PATH
+fish_add_path /Users/emilioramirezmscarua/.spicetify
+set -gx PATH /usr/local/opt/python@3.11/bin $PATH
+set -gx PATH /usr/local/mysql-8.0.34-macos13-arm64/bin/ $PATH
+set PATH $PATH /opt/homebrew/bin
